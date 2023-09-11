fb46464361a505e1c47f7d35c779300e214baab7
计明 <125711775+CJM226@users.noreply.github.com>
2023-09-11 19:14:24 +0800

    Update jsq.js

--- js/jsq.js
+++ js/jsq.js
@@ -12,5 +12,5 @@ const divObj=document.getElementsByClassName('jsq')[0];
         const hour=date.getHours();
         const minite=date.getMinutes();
         const seconds=date.getSeconds();
-        return `${year}-${month}-${day} ${hour}:${minite}:${seconds<10?'0'+seconds:seconds}`
+        return `${year}年${month}月${day}日 ${hour}:${minite}:${seconds<10?'0'+seconds:seconds}`
     }
